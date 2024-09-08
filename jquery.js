53bd5c42fdb1cd87ecd4209b3c616776d67d8c6f
Kaleemullah Ahsan <159533186+kaleemullahahsan@users.noreply.github.com>
2024-09-08 10:19:43 +0500

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,30 +1,46 @@
-$(document).ready(function(){
-    $('.menuIcon').click(function(){
+$(document).ready(function () {
+    $('.menuIcon').click(function () {
         $(this).hide();
         $('.clsmenuIcon').show();
         $('.sidenav').addClass('sidenavactive');
         $('.sidenav div').css({
-            'flex-direction':'row',
+            'flex-direction': 'row',
             'padding': '7px 60px 7px 0px',
             'gap': '14px'
-        }            
+        }
         );
         $('.sidenav div p').css({
             'font-size': '15px'
         });
     });
-    $('.clsmenuIcon').click(function(){
+    $('.clsmenuIcon').click(function () {
         $(this).hide();
         $('.menuIcon').show();
         $('.sidenav').removeClass('sidenavactive');
         $('.sidenav div').css({
-            'flex-direction':'column',
+            'flex-direction': 'column',
             'padding': '8px',
             'gap': '0'
-        }            
+        }
         );
         $('.sidenav div p').css({
             'font-size': '10px'
         });
+    });
+    $('.sbButton').click(function () {
+        $(this).hide();
+        $('.sbdButton').css({
+            "display": "flex"
+        });
+    });
+    $('.sbdButton').click(function () {
+        $(this).hide();
+        $('.sbButton').show();
+    });
+    $('.col2 .btn').click(function () {
+        $(this).toggleClass('btnactive')();
+    });
+    $('.like').click(function(){
+        $(this).toggleClass('btnactive')();
     });
 });
